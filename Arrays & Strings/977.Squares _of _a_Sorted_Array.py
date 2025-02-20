db6a8e1fbedc911bc32100174009af8d83dd99a9
iAdani <guy3102@gmail.com>
2025-02-20 22:19:00 +0200

    Squares of a Sorted Array

--- Arrays & Strings/977.Squares _of _a_Sorted_Array.py
+++ Arrays & Strings/977.Squares _of _a_Sorted_Array.py
@@ -1,5 +1,22 @@
-# O(Nlog(N))
 class Solution:
+    # O(Nlog(N))
     def sortedSquares(self, nums: List[int]) -> List[int]:
         squares = [x * x for x in nums]
         return sorted(squares)
+    
+    # Time: O(N), Space: O(N)
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        nums = [x ** 2 for x in nums]
+
+        pos1, pos2 = 0, n - 1
+        res = []
+        while pos1 <= pos2:
+            if nums[pos1] > nums[pos2]:
+                res.append(nums[pos1])
+                pos1 += 1
+            else:
+                res.append(nums[pos2])
+                pos2 -= 1
+                
+        return res[::-1]
