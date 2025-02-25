9d3bb12fb574fcf2f1114b4c906c3469cca2956e
iAdani <guy3102@gmail.com>
2025-02-25 18:49:31 +0200

    Move Zeroes

---
+++ Arrays & Strings/283.Move_Zeroes.py
@@ -0,0 +1,9 @@
+# Time: O(N), Space: O(1)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        zero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
+     
