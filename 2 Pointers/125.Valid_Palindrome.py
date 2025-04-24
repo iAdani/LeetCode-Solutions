7c756d52e818d80bd8b7462532e0bfe5738a9070
iAdani <guy3102@gmail.com>
2025-04-24 19:28:08 +0300

    Valid Palindrome

--- 2 Pointers/125.Valid_Palindrome.py
+++ 2 Pointers/125.Valid_Palindrome.py
@@ -1,5 +1,5 @@
-# O(N)
 class Solution:
+    # O(N)
     def isPalindrome(self, s: str) -> bool:
         s = "".join(c.lower() for c in s if c.isalnum())        
         start, end = 0, len(s) - 1
@@ -10,4 +10,16 @@ class Solution:
             start += 1
             end -= 1
 
+        return True
+    
+    # O(N)
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s)).lower()
+        start, end = 0, len(s) - 1
+        while start < end:
+            if s[start] != s[end]:
+                return False
+            start += 1
+            end -= 1
+        
         return True
