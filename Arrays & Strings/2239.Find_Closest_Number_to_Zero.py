bb8fdc19f1308913e98917547218ca92b4a05a3c
iAdani <guy3102@gmail.com>
2025-04-24 11:26:17 +0300

    Find Closest Number to Zero

--- Arrays & Strings/2239.Find_Closest_Number_to_Zero.py
+++ Arrays & Strings/2239.Find_Closest_Number_to_Zero.py
@@ -22,3 +22,12 @@ class Solution:
                 negative = num < 0
 
         return -res if negative else res
+    
+    # O(N)
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest = (100001, 0)
+        for num in nums:
+            if abs(num) < closest[0]:
+                closest = (abs(num), num)
+        
+        return closest[0] if closest[1] < 0 and closest[0] in nums else closest[1]
