adffa50741adbd8d87ce746510d8bd4c46eede55
iAdani <guy3102@gmail.com>
2025-05-05 22:21:46 +0300

    Contains Duplicate

--- Hashmaps & Sets/217.Contains_Duplicate.py
+++ Hashmaps & Sets/217.Contains_Duplicate.py
@@ -1,5 +1,9 @@
-# O(N)
 class Solution:
+    # O(N)
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums_set = set(nums)
         return False if len(nums) == len (nums_set) else True
+    
+    # O(N)
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return False if len(nums) == len(set(nums)) else True
