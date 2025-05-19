c13d198c08dbc95c63976792b23d16c5d26aedde
iAdani <guy3102@gmail.com>
2025-05-19 21:29:51 +0300

    Merge Strings Alternately

--- Arrays & Strings/1768.Merge_Strings_Alternately.py
+++ Arrays & Strings/1768.Merge_Strings_Alternately.py
@@ -1,6 +1,8 @@
+import time
+
 class Solution:
     # O(N)
-    def mergeAlternately(self, word1: str, word2: str) -> str:
+    def mergeAlternately1(self, word1: str, word2: str) -> str:
         merged = ''
         idx = 0
         while idx < min(len(word1), len(word2)):
@@ -15,7 +17,7 @@ class Solution:
         return merged
     
     # O(N)
-    def mergeAlternately(self, word1: str, word2: str) -> str:
+    def mergeAlternately2(self, word1: str, word2: str) -> str:
         res = ''
         n, m = len(word1), len(word2)
         i = 0
@@ -33,3 +35,45 @@ class Solution:
         
         return res
     
+    # O(N)
+    def mergeAlternately3(self, word1: str, word2: str) -> str:
+        merged = ''
+        i = 0
+        while i < min(len(word1), len(word2)):
+            merged += word1[i] + word2[i]
+            i += 1
+        
+        merged += word1[i:] + word2[i:]
+        return merged
+
+    # O(N)
+    # Practically A LOT faster than String +=
+    def mergeAlternately4(self, word1: str, word2: str) -> str:
+        result = []
+        i, lenn = 0, min(len(word1), len(word2))
+        while i < lenn:
+            result.append(word1[i])
+            result.append(word2[i])
+            i += 1
+
+        result.append(word1[i:])
+        result.append(word2[i:])
+        return ''.join(result)
+
+
+word1 = 'A' * 999999
+word2 = 'B' * 990009
+
+start_time = time.time()
+res = Solution().mergeAlternately3(word1, word2)
+print(f"--- {(time.time() - start_time)} seconds ---")
+
+start_time = time.time()
+res = Solution().mergeAlternately4(word1, word2)
+print(f"--- {(time.time() - start_time)} seconds ---")
+
+# Result:
+# String += 
+# --- 165.69983291625977 seconds ---
+# List + join
+# --- 0.118988037109375 seconds ---
