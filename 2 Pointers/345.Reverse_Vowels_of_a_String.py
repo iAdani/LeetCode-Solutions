35139a31d341c47b4ed9e301b0a9195fccd14774
iAdani <guy3102@gmail.com>
2025-07-06 23:30:46 +0300

    Reverse Vowels of a String

--- 2 Pointers/345.Reverse_Vowels_of_a_String.py
+++ 2 Pointers/345.Reverse_Vowels_of_a_String.py
@@ -19,3 +19,20 @@ class Solution:
 
         return "".join(s)
     
+
+    # O(N)
+    def reverseVowels(self, s: str) -> str:
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        s = list(s)
+        start, end = 0, len(s) - 1
+        while start < end:
+            if s[start].lower() not in vowels:
+                start += 1
+            elif s[end].lower() not in vowels:
+                end -= 1
+            elif s[start].lower() in vowels and s[end].lower() in vowels:
+                s[start], s[end] = s[end], s[start]
+                start += 1
+                end -= 1
+
+        return "".join(s)
