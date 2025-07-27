cf4f0164c792c91a135126d8fccf050171a575bd
iAdani <guy3102@gmail.com>
2025-07-27 18:55:25 +0300

    Count Good Nodes in Binary Tree

--- Binary Search/1448.Count_Good_Nodes_in_Binary_Tree.py
+++ Binary Search/1448.Count_Good_Nodes_in_Binary_Tree.py
@@ -18,3 +18,18 @@ class Solution:
         DFS(root.left)
         DFS(root.right)
         return count[0]
+
+
+    # O(N)
+    def goodNodes(self, root: TreeNode) -> int:
+        def DFS(root, curr_max):
+            if not root:
+                return 0
+
+            next_max = max(root.val, curr_max)
+            good = DFS(root.left, next_max)
+            good += DFS(root.right, next_max)
+
+            return 1 + good if root.val >= curr_max else good
+
+        return DFS(root, root.val)
