37cf35c36cf0db9cf04093e2eb0686bc4b7b54bb
iAdani <guy3102@gmail.com>
2026-06-07 21:54:57 +0300

    Best Time to Buy and Sell Stock

--- Arrays & Strings/121.Best_Time_to_Buy_and_Sell_Stock.py
+++ Arrays & Strings/121.Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,5 +1,5 @@
-# O(N)
 class Solution:
+    # O(N)
     def maxProfit(self, prices: List[int]) -> int:
         maximum = 0
         lowest = prices[0]
@@ -10,3 +10,12 @@ class Solution:
             lowest = min(lowest, price)
 
         return maximum
+    
+    # O(N)
+    # Just nicer
+    def maxProfit(self, prices: List[int]) -> int:
+        min_seen, max_profit = prices[0], 0
+        for price in prices:
+            max_profit = max(max_profit, price - min_seen)
+            min_seen = min(min_seen, price)
+        return max_profit
