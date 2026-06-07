7ad547c447d9a8ef10ba0bee0a33255d3f4ecf1c
iAdani <guy3102@gmail.com>
2026-06-07 21:05:44 +0300

    Valid Parentheses

--- Stacks/20.Valid_Parentheses.py
+++ Stacks/20.Valid_Parentheses.py
@@ -14,3 +14,34 @@ class Solution:
         
         return not stk
             
+    # O(N)
+    # Faster than the first
+    def isValid(self, s: str) -> bool:
+        stk = []
+        for c in s:
+            if c in "([{":
+                stk.append(c)
+            else:
+                if not stk:
+                    return False
+                h = stk.pop()
+                if (c == ')' and h != '(') or (c == ']' and h != '[') or (c == '}' and h != '{'):
+                    return False
+                    
+        return not stk
+    
+    # O(N)
+    # Better if
+    def isValid(self, s: str) -> bool:
+        stk = []
+        for c in s:
+            if c in "([{":
+                stk.append(c)
+            else:
+                if not stk:
+                    return False
+                h = stk.pop()
+                if [h, c] not in [['(', ')'], ['[', ']'], ['{', '}']]:
+                    return False
+                    
+        return not stk
