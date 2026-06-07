f283f1ca9286b9bae84330fd9b3813c3b5b8c7c2
iAdani <guy3102@gmail.com>
2026-06-07 17:05:32 +0300

    Valid Anagram

--- Hashmaps & Sets/242.Valid_Anagram.py
+++ Hashmaps & Sets/242.Valid_Anagram.py
@@ -1,5 +1,5 @@
-# Time: O(n), Space: O(n + m)
 class Solution:
+    # Time: O(n), Space: O(n + m)
     def isAnagram(self, s: str, t: str) -> bool:
         freq = {}
         if len(s) != len(t) or set(s) != set(t):
@@ -18,3 +18,24 @@ class Solution:
         if all(x == 0 for x in freq.values()):
             return True
         return False
+
+    # Time: O(n), Space: O(n + m)
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq = {}
+        if len(s) != len(t) or set(s) != set(t):
+            return False
+        
+        for i in range(len(s)):
+            freq[s[i]] = freq.get(s[i], 0) + 1
+            freq[t[i]] = freq.get(t[i], 0) - 1
+        
+        if all(x == 0 for x in freq.values()):
+            return True
+        return False
+    
+    # Since s and t consist of lowercase English letters
+    # only, the freq can only have O(1) keys, and each 
+    # key can only have a maximum of size(int) bits
+    # representation. Therefore in that case, the space 
+    # complexity is O(1), and in the general case, without
+    # the length constraint, it is O(log(N)).
