94495f9bca30fee625e441631ae6ccf26747d49c
iAdani <guy3102@gmail.com>
2026-06-07 22:33:15 +0300

    Move Zeroes

--- 2 Pointers/283.Move_Zeroes.py
+++ 2 Pointers/283.Move_Zeroes.py
@@ -1,9 +1,33 @@
-# Time: O(N), Space: O(1)
 class Solution:
+    # Time: O(N), Space: O(1)
     def moveZeroes(self, nums: List[int]) -> None:
-        zero = 0
+        pos = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i], nums[zero] = nums[zero], nums[i]
-                zero += 1
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
      
+    
+    # Time: O(N), Space: O(1)
+    def moveZeroes(self, nums: List[int]) -> None:
+        zero_count = 0
+        for i, num in enumerate(nums):
+            if num == 0:
+                zero_count += 1
+            else:
+                nums[i - zero_count] = num
+        
+        for i in range(zero_count, 0, -1):
+            nums[-i] = 0
+
+    
+    # Time: O(N), Space: O(1)
+    # i != pos saves time even though both are O(N)
+    def moveZeroes(self, nums: List[int]) -> None:
+        pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if i != pos:
+                    nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
+            
